16abe073945e54694ed83beabcdc282d792fa63f
duart38 <duartsnel@icloud.com>
2022-02-17 19:48:09 +0100

    initial scratch pad

---
+++ mod.ts
@@ -0,0 +1,95 @@
+export type VPinNumber = 2 | 3 | 4 | 17 | 27 | 22 | 10 | 9 | 11 | 0 | 5 | 6 | 13 | 19 | 26 | 14 | 15 | 18 | 23 | 24 | 25 | 8 | 7 | 1 | 12 | 16 | 20 | 21;
+export enum PinDirection {
+    IN = "in", OUT= "out"
+}
+
+async function runEchoReplaceCommand(value: string, toFile: string){
+    await Deno.run({
+        cmd: ["bash","-c", `echo ${value} > ${toFile}`]
+    }).status()
+}
+
+function unexportOnGC(pin: Pin){
+    const registry = new FinalizationRegistry((heldValue: number) => {
+        Pin.unexportPin(heldValue)
+    });
+    registry.register(pin, pin.number);
+}
+
+export interface Options {
+    /**
+     * Wether we should un-export the pin if a termination OS signal is captured.
+     */
+    unexportOnSig: boolean
+}
+const defaultOptions: Options = {
+    unexportOnSig: true
+}
+
+export class Pin {
+    readonly number: VPinNumber;
+    ready: Promise<void>;
+
+    // TODO: identify safety of pin with parameter? e.g. unsafe: bool
+    // TODO: implement options
+    constructor(number: VPinNumber, direction: PinDirection, options: Options = defaultOptions){
+        this.number = number;
+        this.ready = new Promise((resolve)=>{
+            Pin.exportPin(this).then(async ()=>{
+                await this.setDirection(direction);
+                resolve()
+            })
+        })
+
+        unexportOnGC(this);
+    }
+
+
+    // TODO: If an output pin, set the level to low or high.
+    // TODO: If an input pin, read the pin's level (low or high).
+
+    // TODO: what about analogue pin support
+
+    async readPin(){
+
+    }
+
+    /**
+     * 
+     * @param value 0 for low, 1 for high
+     */
+    async setPin(value: number){
+        await this.ready
+        await runEchoReplaceCommand(value.toString(), `/sys/class/gpio/gpio${this.number}/value`)
+    }
+
+    async setDirection(d: PinDirection) {
+        await runEchoReplaceCommand(d, `/sys/class/gpio/gpio${this.number.toString()}/direction`)
+    }
+
+
+    async getDirection(): Promise<PinDirection> {
+        return new TextDecoder().decode(
+            await Deno
+            .run({cmd: ["cat", `/sys/class/gpio/gpio${this.number}/direction`], stdout: 'piped'})
+            .output()
+        ).includes('out') ? PinDirection.OUT : PinDirection.IN;
+    }
+
+    static async exportPin(pin: Pin){
+        await runEchoReplaceCommand(pin.number.toString(), "/sys/class/gpio/export")
+    }
+    
+    static async unexportPin(pin: Pin | number){
+        const pinNumber: string = (typeof pin === "number" ? pin : pin.number).toString();
+        await runEchoReplaceCommand(pinNumber, "/sys/class/gpio/unexport")
+    }
+
+    static async isExported(pin: Pin): Promise<boolean> {
+        return new TextDecoder()
+        .decode(await Deno.run({cmd: ["ls", "/sys/class/gpio"], stdout: "piped"}).output())
+        .includes(`gpio${pin.number}`);
+    }
+}
+
+
